826a941637ddd19f0847c6f30cf523dd3c2c41de
jku20 <jku-ben20@stuy.edu>
2020-12-02 17:24:31 -0500

    used dynamic memory so it isn't reliant on a global

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
-
-char *out[6];
+#include <stdlib.h>
 
 char **parse_args(char *line) {
+    char **out = calloc(sizeof(char*),6);
     char **p = out;
     while(*p = strsep(&line," "),*(p++));
     return out;
@@ -14,6 +14,8 @@ int main() {
     char line[] = "ls -a -l";
     char **args = parse_args(line);
     execvp(args[0],args);
+    //args will be reclaimed by execvp so free is not needed
+    //
     //hello return, I am sorry you don't get run, it is ok
     return 0;
 }
